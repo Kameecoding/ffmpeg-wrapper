e11e0c7e1c9d2b840e2201892553d0400bb0ebdf
Kamee <andrej.kovac.ggc@gmail.com>
2017-08-26 11:34:41 +0200

    Added base implementation of ffprobe and subtitle extractor

---
+++ src/main/java/com/kameecoding/filebot/SubtitleExtractor.java
@@ -0,0 +1,20 @@
+package com.kameecoding.filebot;
+
+/**
+ * Created by Andrej Kovac (kameecoding) <andrej.kovac.ggc@gmail.com> on 2017-08-20.
+ */
+public class SubtitleExtractor implements Runnable {
+
+    private SubtitleExtractor() {
+
+    }
+
+    public static SubtitleExtractor newInstance() {
+        return new SubtitleExtractor();
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
