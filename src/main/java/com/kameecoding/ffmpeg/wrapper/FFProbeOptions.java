7b39c4e3131ba94bca361a2109d06c0769a48021
kameecooding <andrej.kovac.ggc@gmail.com>
2017-10-07 17:05:11 +0200

    added some basic ffprobe option enumeratioon

---
+++ src/main/java/com/kameecoding/ffmpeg/wrapper/FFProbeOptions.java
@@ -0,0 +1,38 @@
+package com.kameecoding.ffmpeg.wrapper;
+
+/**
+ * Created by Andrej Kovac (kameecoding) <andrej.kovac.ggc@gmail.com> on 06/10/2017.
+ */
+public enum FFProbeOptions {
+	SHOW_FORMAT("-show_format", ""),
+	SHOW_STREAMS("-show_streams", ""),
+	OUTPUT_FORMAT("-of", "", true);
+
+	private final String opt;
+	private final String description;
+	private final boolean hasArgument;
+
+	FFProbeOptions(String opt, String description, boolean hasArgument) {
+		this.opt = opt;
+		this.description = description;
+		this.hasArgument = hasArgument;
+	}
+
+	FFProbeOptions(String opt, String description) {
+		this.opt = opt;
+		this.description = description;
+		this.hasArgument = false;
+	}
+
+	public String getOpt() {
+		return opt;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public boolean hasArgument() {
+		return hasArgument;
+	}
+}
