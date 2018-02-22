e1a151be1717c75f422dc46bab883064a39c4ed5
Kameecoding <andrej.kovac.ggc@gmail.com>
2018-02-22 20:55:40 +0100

    feat(#2): Added subtitle extractor
    Closes #2

---
+++ src/main/java/com/kameecoding/ffmpeg/callable/SubtitleExtractResult.java
@@ -0,0 +1,23 @@
+/*
+ * Some open source application
+ *
+ * Copyright 2018 by it's authors.
+ *
+ * Licensed under GNU General Public License 3.0 or later.
+ * Some rights reserved. See LICENSE, AUTHORS.
+ *
+ * @license GPL-3.0+ <https://opensource.org/licenses/GPL-3.0>
+ */
+package com.kameecoding.ffmpeg.callable;
+
+import java.io.File;
+
+public class SubtitleExtractResult extends OperationResult {
+    File output;
+
+    SubtitleExtractResult() {}
+
+    public File getOutput() {
+        return output;
+    }
+}
---
+++ src/main/java/com/kameecoding/ffmpeg/callable/SubtitleExtractor.java
@@ -0,0 +1,93 @@
+package com.kameecoding.ffmpeg.callable;
+
+import com.kameecoding.ffmpeg.FFmpeg;
+import com.kameecoding.ffmpeg.dto.SubtitleStream;
+import com.kameecoding.ffmpeg.enums.ResultType;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+public class SubtitleExtractor implements Callable<SubtitleExtractResult> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MetadataUpdater.class);
+    private File executable;
+    private File logFile;
+    private List<String> argList = new ArrayList<>();
+    private File outputFile;
+
+    private SubtitleExtractor() {}
+
+    @Override
+    public SubtitleExtractResult call() {
+        SubtitleExtractResult result = new SubtitleExtractResult();
+        result.result = ResultType.SUCCESS;
+        FFmpeg extractor = FFmpeg.newInstance(executable, argList, logFile);
+        extractor.run();
+        if (outputFile.exists() && FileUtils.sizeOf(outputFile) > 2L) {
+            result.output = outputFile;
+        } else {
+            result.result = ResultType.FAILED;
+            result.errorMessages.add("Failed to extract subtitle");
+            LOGGER.error("Failed to extract subtitle");
+        }
+
+        return result;
+    }
+
+    public static class SubtitleExtractorConfigurator {
+        private SubtitleExtractor subtitleExtractor = new SubtitleExtractor();
+        private File input;
+        private File output;
+        private SubtitleStream stream;
+
+        public SubtitleExtractorConfigurator(File executable, File input, SubtitleStream stream) {
+            subtitleExtractor.executable = executable;
+            this.input = input;
+            this.stream = stream;
+        }
+
+        public SubtitleExtractorConfigurator logfile(File logfile) {
+            subtitleExtractor.logFile = logfile;
+            return this;
+        }
+
+        public SubtitleExtractorConfigurator output(File output) {
+            this.output = output;
+            return this;
+        }
+
+        public SubtitleExtractor configure() {
+            String basePath = FilenameUtils.getFullPath(input.getAbsolutePath());
+            if (output != null) {
+                basePath = FilenameUtils.getFullPath(output.getAbsolutePath());
+            }
+            StringBuilder output = new StringBuilder(basePath);
+            output.append(FilenameUtils.getBaseName(input.getAbsolutePath()));
+            output.append(".");
+            output.append(stream.getLanguage().getAlpha3B());
+            boolean forced = stream.isForced();
+            if (forced) {
+                output.append(".forced");
+            }
+            output.append(".srt");
+            String outputPath = output.toString();
+            LOGGER.info("Extracting subtitle to: {}", outputPath);
+            subtitleExtractor.outputFile = new File(output.toString());
+            subtitleExtractor.argList.add("-y");
+            subtitleExtractor.argList.add("-i");
+            subtitleExtractor.argList.add(input.getAbsolutePath());
+            subtitleExtractor.argList.add("-c");
+            subtitleExtractor.argList.add("copy");
+            subtitleExtractor.argList.add("-map");
+            subtitleExtractor.argList.add("0:" + String.valueOf(stream.getStreamMapping()));
+            subtitleExtractor.argList.add(subtitleExtractor.outputFile.getAbsolutePath());
+
+            return subtitleExtractor;
+        }
+    }
+}
